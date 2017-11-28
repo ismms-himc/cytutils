0bb8b43d7eb0c944bdf7218ad3b382dd13f761b8
El-ad David Amir <el.ad.david.amir@gmail.com>
2017-11-28 08:54:31 -0500

    Dup handling is now optional

--- R/channel_rename.R
+++ R/channel_rename.R
@@ -6,11 +6,19 @@
 #' Readme at "FCS Channel Renaming" for instructions.
 #'
 #' @param path Directory where FCS files are located.
+#' @param dup_handling How to handle masses with duplicates name or desc.
+#' Options are "stop", "warning", and NULL.
 #' @param verbose If TRUE, script will report progress to console.
 #' @export
-channelRename <- function(path, verbose = TRUE) {
+channelRename <- function(path, dup_handling = "warning", verbose = TRUE) {
   filenames <- file.path(path, dir(path, "\\.fcs$"))
   if (length(filenames) == 0) stop("could not find any FCS files at given path")
+
+  if (!is.null(dup_handling)) {
+    if (!(dup_handling %in% c("warning", "stop"))) {
+      stop("dup_handling not in allowed values (NULL, warning, or stop)")
+    }
+  }
 
   csv_filename <- file.path(path, "channel_rename.csv")
   if (file.exists(csv_filename)) {
@@ -22,10 +30,18 @@ channelRename <- function(path, verbose = TRUE) {
     channels$name[is.na(channels$name)] <- ""
     channels$desc[is.na(channels$desc)] <- ""
 
-    dups <- .findMassDups(channels)
-    if (length(dups) > 0) {
-      stop(paste0("the following masses have duplicate new_name or new_desc: ",
-                  paste(dups, collapse = ", ")))
+    if (!is.null(dup_handling)) {
+      dups <- .findMassDups(channels)
+      if (length(dups) > 0) {
+        msg <-
+          paste0("the following masses have duplicate new_name or new_desc: ",
+                 paste(dups, collapse = ", "))
+        if (dup_handling == "warning") {
+          warning(msg)
+        } else if (dup_handling == "stop") {
+          stop(msg)
+        }
+      }
     }
 
     dest_path <- file.path(path, "channel_rename")
