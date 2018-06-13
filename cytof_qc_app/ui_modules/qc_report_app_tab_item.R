5c5a18c6de5a8617da229ce675d0ccad9e1a4ba8
Melanie Davila <davilam91@gmail.com>
2018-06-13 11:30:32 -0400

    Add qc_report_app_tab_item with output directory and FCS file selection features.

---
+++ cytof_qc_app/ui_modules/qc_report_app_tab_item.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(shinydashboard)
+library(shinyFiles)
+
+qc_report_app_tab_item <- tabItem(tabName = "qc_report_app",
+                      h2("CyTOF QC Report Generator",
+                        style = "padding-left: 15px"),
+                      fluidRow(
+                        sidebarPanel(
+                          p(paste("Please select the location you would like to", 
+                          "export the QC report to. A message will appear to the",
+                          "right when the location has successfully been",
+                          "selected.")),
+                          shinyDirButton(id="sample_background_report_dir", 
+                                        label="Choose directory", 
+                                        title="Select directory" ),
+                          br(),
+                          br(),
+                          br(),
+                          p(paste("Please upload CyTOF FCS file(s) to run a QC report.",
+                                    "Report results will be exported in a",
+                                    "cytof_qc_report.csv in the location chosen",
+                                    "above.")),
+                          fileInput(
+                            # NB: Regarding all inputId's, inputId is assigned to 
+                            # singular "x_file" because under most circumstances, a single
+                            # file will be uploaded; however, multiple files may be 
+                            # uploaded. Whenever a file upload completes, the corresponding 
+                            # input variable is set to a dataframe. This dataframe contains 
+                            # one row for each selected file. 
+                            inputId = "cytof_qc_file",
+                            label = paste("Please do not exit the application until a status", 
+                                          "message appears to the right of this",
+                                          "section or data export may be compromised."),
+                            multiple = TRUE,
+                            accept = c(".fcs", "fcs")
+                          )
+                        ),
+                        mainPanel(
+                          htmlOutput("cytof_qc_invalid_file_type"),
+                          # htmlOutput("cytof_qc_google_sheet_not_found"),
+                          htmlOutput("cytof_qc_fcs_file_import_errors"),
+                          htmlOutput("cytof_qc_pre_processing_errors"),
+                          htmlOutput("cytof_qc_report_generation_errors"),
+                          # htmlOutput("cytof_qc_google_drive_data_transfer_errors"),
+                          htmlOutput("cytof_qc_successful_completions")
+                        )
+                      ),
+                      uiOutput("gating_inspection_and_visualization")
+                  )
