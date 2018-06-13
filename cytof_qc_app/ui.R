00630cc82b4cfa077b23d6683d75cd07e77f631b
Melanie Davila <davilam91@gmail.com>
2018-06-13 11:15:08 -0400

    Update UI file with tab for cytof qc feature

--- cytof_qc_app/ui.R
+++ cytof_qc_app/ui.R
@@ -7,6 +7,9 @@ ui <- dashboardPage(
   dashboardHeader(title = "CyTOF QC App"),
   dashboardSidebar(
     sidebarMenu(
+      menuItem("CyTOF QC Report Generator", 
+          tabName = "qc_report_app", 
+          icon = icon("check-circle-o")),
       menuItem("Sample Background Tracking", 
                 tabName = "sample_background_app",
                 icon = icon("flask"))
@@ -14,6 +17,7 @@ ui <- dashboardPage(
   ),
   dashboardBody(
     tabItems(
+      qc_report_app_tab_item,
       sample_background_app_tab_item
     )
   )
