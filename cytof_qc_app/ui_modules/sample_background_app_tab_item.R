316cddc8bd2efa938ab49e3acbeccaca1cd7433e
Melanie Davila <davilam91@gmail.com>
2019-01-14 15:30:05 -0500

    Update UI module to contain section for rendering sample background report table

--- cytof_qc_app/ui_modules/sample_background_app_tab_item.R
+++ cytof_qc_app/ui_modules/sample_background_app_tab_item.R
@@ -5,36 +5,39 @@ library(shinyFiles)
 sample_background_app_tab_item <-  tabItem(tabName = "sample_background_app",
       h2("Sample Background Tracking",
         style = "padding-left: 15px"),
-      sidebarPanel(
-          p(paste("A separate sample background report will be generated for each",
-                    "uploaded FCS file.")),
-          p(paste("Please select the location you would like to export the sample",
-                    "background report(s) to. A message will appear to the right when",
-                    "the location has successfully been selected.")),
-          shinyDirButton(id="sample_background_report_dir", label="Choose directory", title="Select directory" ),
-          br(),
-          br(),
-          br(),
-          p(paste(
-            "Please upload sample background FCS file(s). Data from the",
-            "uploaded file(s) will be pre-processed and exported.")),
-          fileInput(
-            inputId = "sample_background_file",
-            label = paste("Please do not exit the application until a sample background report-specific status",
-              "message appears to the right of this section or QC report export",
-              "may be compromised."),
-            multiple = TRUE,
-            accept = c(".fcs", "fcs")
-          )
+        fluidRow(
+          sidebarPanel(
+              p(paste("A separate sample background report will be generated for each",
+                        "uploaded FCS file.")),
+              p(paste("Please select the location you would like to export the sample",
+                        "background report(s) to. A message will appear to the right when",
+                        "the location has successfully been selected.")),
+              shinyDirButton(id="sample_background_report_dir", label="Choose directory", title="Select directory" ),
+              br(),
+              br(),
+              br(),
+              p(paste(
+                "Please upload sample background FCS file(s). Data from the",
+                "uploaded file(s) will be pre-processed and exported.")),
+              fileInput(
+                inputId = "sample_background_file",
+                label = paste("Please do not exit the application until a sample background report-specific status",
+                  "message appears to the right of this section or QC report export",
+                  "may be compromised."),
+                multiple = TRUE,
+                accept = c(".fcs", "fcs")
+              )
+            ),
+            mainPanel(
+              htmlOutput("sample_background_report_dir_selection_status"),
+              htmlOutput("sample_background_invalid_file_type"),
+              htmlOutput("sample_background_fcs_file_import_errors"),
+              htmlOutput("sample_background_pre_processing_errors"),
+              htmlOutput("sample_background_report_generation_errors"),
+              htmlOutput("sample_background_report_successful_completions"),
+              htmlOutput("sample_background_report_export_successes"),
+              htmlOutput("sample_background_report_export_errors")
+            )
         ),
-        mainPanel(
-          htmlOutput("sample_background_report_dir_selection_status"),
-          htmlOutput("sample_background_invalid_file_type"),
-          htmlOutput("sample_background_fcs_file_import_errors"),
-          htmlOutput("sample_background_pre_processing_errors"),
-          htmlOutput("sample_background_report_generation_errors"),
-          htmlOutput("sample_background_report_successful_completions"),
-          htmlOutput("sample_background_report_export_successes"),
-          htmlOutput("sample_background_report_export_errors")
-        )
-      )
+        uiOutput('sample_background_report_table_section')
+    )
